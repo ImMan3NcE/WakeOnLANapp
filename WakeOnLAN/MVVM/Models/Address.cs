f9b694ddde5ca842d1303a63dd204f4ad03e560b
scule <scule>
2024-11-29 21:29:35 +0100

    Model: ConnectionAddress

---
+++ WakeOnLAN/MVVM/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeOnLAN.MVVM.Models
+{
+    [Table("ConnectionAddress")]
+    public class Address
+    {
+        [PrimaryKey,AutoIncrement]
+        public int IdAddress { get; set; }
+        public string Name { get; set; }
+        public string MacAddress { get; set; }
+        public string IpAddress { get; set; }
+        public string Port { get; set; }
+    }
+}
